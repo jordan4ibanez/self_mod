abceda08c62d957853ce6d5520face4589f1ec66
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-02 22:05:13 -0500

    So we'd have it set up like this

--- mods/api.d
+++ mods/api.d
@@ -1,2 +1,35 @@
 module mods.api;
 
+class Mob {
+    string modName = "undefined";
+    string name = "undefined";
+    int hp = 0;
+}
+
+static final const class Api {
+static:
+private:
+
+    // In this sample, we would be creating mobs for this imaginary game.
+
+    Mob[string] database;
+
+    public void registerMob(Mob mob) {
+        if (mob.name == "undefined") {
+            throw new Error("You must name your mob.");
+        }
+
+        if (mob.modName == "undefined") {
+            throw new Error("You must define the mod in which your mob comes from.");
+        }
+
+        if (mob.name in database) {
+            throw new Error(
+                "Mob name " ~ conflicts ~ " from mod " ~ database[mob.name].modName ~ " with other mod " ~ mob
+                    .modName);
+        }
+
+        database[mob.name] = mob;
+    }
+
+}
