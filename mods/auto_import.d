68bc48c5d6a3c8d8e7024852d9a70eebd30ab838
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-02 22:31:45 -0500

    I am out of my mind

--- mods/auto_import.d
+++ mods/auto_import.d
@@ -1,7 +1,9 @@
 module mods.auto_import;
 
+import std.array;
 import std.file;
 import std.stdio;
+import std.string;
 
 /*
 This file literally just modified the api.d file.
@@ -10,9 +12,22 @@ This file literally just modified the api.d file.
 void main() {
     writeln("AUTO IMPORT: Automating imports.");
 
+    string[] importList = [];
+
     foreach (filename; dirEntries("mods/", SpanMode.shallow)) {
         if (isDir!string(filename)) {
-            writeln("folder: ", filename);
+            // writeln("folder: ", filename);
+
+            string target = filename ~ "/init.d";
+
+            if (isFile!string(target)) {
+                // Turn it into a module path and chop the [.d] off it.
+                string thisImport = target.replace("/", ".");
+                thisImport = thisImport[0 .. (thisImport.length) - 2];
+
+                importList ~= thisImport;
+
+            }
         }
     }
 }
--- mods/my_cool_mod/init.d
+++ mods/my_cool_mod/init.d
@@ -2,6 +2,8 @@ module mods.my_cool_mod.init;
 
 import std.stdio;
 
+// It must follow your module name.
+// So here my_cool_mod.init becomes myCoolModInit.
 void myCoolModInit() {
 
 }
