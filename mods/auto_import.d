9dba694f85ab7a0fa9e03337471415b556ceb6f5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-02 22:58:14 -0500

    I'll finish this insanity tomorrow

--- mods/auto_import.d
+++ mods/auto_import.d
@@ -1,6 +1,8 @@
 module mods.auto_import;
 
+import std.algorithm.mutation;
 import std.array;
+import std.conv;
 import std.file;
 import std.stdio;
 import std.string;
@@ -24,8 +26,39 @@ void main() {
                 // Turn it into a module path and chop the [.d] off it.
                 string thisImport = target.replace("/", ".");
                 thisImport = thisImport[0 .. (thisImport.length) - 2];
-
                 importList ~= thisImport;
+
+                string thisFunctionName = thisImport;
+
+                // Remove the [mods.]
+                {
+                    char[] temp = to!(char[])(thisFunctionName);
+                    temp = temp.remove(0, 1, 2, 3, 4);
+                    thisFunctionName = temp.idup;
+                }
+
+                // Take care of the periods.
+                while (long x = thisFunctionName.indexOf(".")) {
+                    if (x <= 0) {
+                        break;
+                    }
+                    char[] temp = to!(char[])(thisFunctionName);
+                    temp[x + 1 .. x + 2] = temp[x + 1 .. x + 2].toUpper();
+                    temp = temp.remove(x);
+                    thisFunctionName = temp.idup;
+                }
+
+                // Take care of the underscore.
+                while (long x = thisFunctionName.indexOf("_")) {
+                    if (x <= 0) {
+                        break;
+                    }
+                    char[] temp = to!(char[])(thisFunctionName);
+                    temp[x + 1 .. x + 2] = temp[x + 1 .. x + 2].toUpper();
+                    temp = temp.remove(x);
+                    thisFunctionName = temp.idup;
+                }
+                writeln(thisFunctionName);
 
             }
         }
