06a3398e7c26891ef6c07fecfcbb114e4c182c29
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-03 11:58:19 -0500

    So I wrote this

---
+++ source/mods/desert/init.d
@@ -0,0 +1,25 @@
+module mods.desert.init;
+
+import mods.api;
+
+// In this one I just kind of extend the initial mob so I don't have to define the
+// mod in which they came from over and over.
+class DesertMob : Mob {
+    this() {
+        this.modName = "desert";
+    }
+}
+
+// It must follow your module name.
+// So here desert becomes desertMain.
+// This is basically symbolizing "void main" for your mod.
+void desertMain() {
+
+    // Why would you have a cactus as a mob?
+
+    DesertMob cactus = new DesertMob();
+    cactus.name = "cactus";
+
+    Api.registerMob(cactus);
+
+}
