2d73a42ee92971585440ffad3fee1c645b0d0965
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-02-03 10:27:32 -0500

    Update init.d

--- mods/my_cool_mod/init.d
+++ mods/my_cool_mod/init.d
@@ -3,7 +3,8 @@ module mods.my_cool_mod.init;
 import std.stdio;
 
 // It must follow your module name.
-// So here my_cool_mod.init becomes myCoolModInit.
-void myCoolModInit() {
-
+// So here my_cool_mod becomes myCoolMod.
+// This is basically symbolizing "void main" for your mod.
+void myCoolMod() {
+    writeln("I am a cool mod");
 }
